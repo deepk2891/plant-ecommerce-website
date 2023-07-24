e1ea86a214fee0f3759b799d4723ff46238cb42f
deepk2891 <deepk2891@gmail.com>
2023-07-24 20:36:59 +0530

    completed footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,7 @@ $(document).ready(function () {
     $('.toggle-sidebar-btn').click(function () {
         $(this).toggleClass('active');
         $('.sidebar').toggleClass('move-sidebar');
+        $('.main').toggleClass('move-main');
+        $('.footer').toggleClass('move-footer');
     });
 });
