e4477be93222f8c1f023d50539c09d5979979713
deepk2891 <deepk2891@gmail.com>
2023-07-25 14:32:03 +0530

    Update sidebar navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $('.toggle-sidebar-btn').click(function () {
         $(this).toggleClass('active');
         $('.sidebar').toggleClass('move-sidebar');
-        $('.main').toggleClass('move-main');
-        $('.footer').toggleClass('move-footer');
+        $(this).toggleClass("ri-side-bar-line ri-side-bar-fill");
     });
 });
